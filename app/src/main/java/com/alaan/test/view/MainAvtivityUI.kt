95921ef69ee726156c6cd9ce96949e9ddff8daab
Rawand Hamasharif <rawand.p@gmail.com>
2020-01-25 15:43:59 +0330

    last

--- app/src/main/java/com/alaan/test/view/MainAvtivityUI.kt
+++ app/src/main/java/com/alaan/test/view/MainAvtivityUI.kt
@@ -15,6 +15,7 @@ class MainAvtivityUI(orientation1: Int) : AnkoComponent<MainActivity> {
 
     var orientation1: Int?
     var arrButton = mutableListOf<Button>()
+    var count = 0
 
     init {
         this.orientation1 = orientation1
@@ -26,29 +27,30 @@ class MainAvtivityUI(orientation1: Int) : AnkoComponent<MainActivity> {
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
 
 
-
-
         verticalLayout {
 
             id = R.id.idParent
             backgroundColor = Color.WHITE
 
-            orientation = if (orientation1 == 1) LinearLayout.HORIZONTAL else LinearLayout.VERTICAL
-
-
+           // orientation = if (orientation1 == 1) LinearLayout.HORIZONTAL else LinearLayout.VERTICAL
 
             buttonCustomView(this, resources.getString(R.string.MOVE), Color.RED)
                 .lparams(width = wrapContent, height = wrapContent) {
                     margin = 20
 
                 }.onClick {
+                    
+                  addView(linearLayout {
+                      id = 10
+                        backgroundColor = Color.RED
+                        count = count + 1
+                        arrButton.add(buttonCustomView(this,"button : " + arrButton.size,Color.BLUE).lparams(
+                            wrapContent, wrapContent) {
+                            margin = 10
+                        })
 
-                    arrButton.add( buttonCustomView(this@verticalLayout, "button : "+arrButton.size, Color.BLUE).lparams(
-                        wrapContent,
-                        wrapContent
-                    ) {
-                        margin = 10
                     })
+
 
                 }
 
@@ -86,16 +88,5 @@ class MainAvtivityUI(orientation1: Int) : AnkoComponent<MainActivity> {
             backgroundColor = color
         }
     }
-
-
-    fun linearCustomer(ui: @AnkoViewDslMarker _LinearLayout, array: List<Button>):LinearLayout{
-
-        return ui.verticalLayout{
-            orientation = LinearLayout.HORIZONTAL
-            lparams(width = wrapContent,height = wrapContent)
-        }
-
-    }
-
 }
 
